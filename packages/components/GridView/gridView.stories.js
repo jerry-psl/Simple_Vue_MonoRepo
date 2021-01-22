7e23a4ccffe05a6560c75c5154a723ac6a82001d
j3rry320 <rutu.satpathy44@gmail.com>
2021-01-22 15:08:20 +0530

    Basic Setup with storybook done.

---
+++ packages/components/GridView/gridView.stories.js
@@ -0,0 +1,8 @@
+// import Vue from 'vue';
+import { storiesOf } from '@storybook/vue';
+import GridView from './gridView.vue';
+
+storiesOf('GridView', module).add('as a component', () => ({
+  components: { GridView },
+  template: '<GridView></GridView>',
+}));
---
+++ packages/components/GridView/gridView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>I am working fine for now</div>
+</template>
+<script>
+export default {
+  name: 'GridView',
+};
+</script>
